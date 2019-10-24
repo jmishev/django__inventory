1c292b915c34ac8e18422ea10464114f01dee0b1
jmishev <53108707+jmishev@users.noreply.github.com>
2019-10-24 12:16:12 +0300

    host allowed

--- revision_project/settings.py
+++ revision_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://inventorybg.herokuapp.com/']
 
 
 # Application definition
